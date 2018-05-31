ab357dee4b9f664706389f19dff6df24bd466917
ADA <ada3000@gmail.com>
2018-05-31 16:11:48 +0300

    Create TypeScript.js

---
+++ VisualStudio/TypeScript.js
@@ -0,0 +1,3 @@
+### Не работает подсветка
+Симптом: в свойствах проекта-> TypeScript Build отображается ошибка
+Решение: Удалить папку: C:\Users\<userName>\AppData\Local\Microsoft\VisualStudio\14.0\ComponentModelCache
